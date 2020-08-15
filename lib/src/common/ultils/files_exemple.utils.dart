0970ca7cfe0daf0dd35ec7ca624427830403e69e
Cristiano nascimento <cristiano@proserv.net.br>
2020-08-15 03:24:25 -0300

    create Text Widget in Files exemples

--- lib/src/common/ultils/files_exemple.utils.dart
+++ lib/src/common/ultils/files_exemple.utils.dart
@@ -2,6 +2,7 @@ class FileExempleUtils {
   static String createTextController(String name) {
     return '''
 import 'package:get/get.dart';
+
 class ${name}Controller extends GetxController{
   //todo: ${name}Controller
 }
@@ -9,8 +10,10 @@ class ${name}Controller extends GetxController{
   }
 
   static String createTextScreen(String name) {
-    return '''import 'package:flutter/material.dart';
+    return '''
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 class ${name}Screen extends GetView<${name}Controller> {
   @override
   Widget build(BuildContext context) {
@@ -20,12 +23,28 @@ class ${name}Screen extends GetView<${name}Controller> {
 ''';
   }
 
+  static String createTextWidget(String name) {
+    return '''
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ${name}Widget extends GetWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+''';
+  }
+
   static String createTextRoutes() {
-    return '''class Routes {
+    return '''
+class Routes {
   static Future<String> get initialRoute async {
     //todo: create method
     return null;
   }
+  
 }''';
   }
 }
